917ed26a6f639b86cac535a6df864831b10f901d
Eric Taylor <eric.taylor1012@gmail.com>
2023-03-11 20:28:43 -0800

    rendered elements

--- src/App.js
+++ src/App.js
@@ -5,23 +5,25 @@ import React from 'react';
 class App extends React.Component {
   constructor(props){
     super(props)
+    this.state = {
+      saved: 0,
+      month: 500
+    }
   }
   render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{display: 'flex', justifyContent: 'space-evenly', width: "100vw"}}>
+          <div>
+            <h1>{this.state.month}</h1>
+            <h3>Monthly Budget</h3>
+          </div>
+          <div>
+            <h1>{this.state.saved}</h1>
+            <h3>Amount Saved</h3>
+          </div>
+        </div>
       </header>
     </div>
   );
