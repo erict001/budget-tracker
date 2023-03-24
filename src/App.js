6175e3c9b2fc09796839a64191485bbb7368b598
Eric Taylor <eric.taylor1012@gmail.com>
2023-03-24 10:05:53 -0700

    added date tracker and moved handleChange into input fields

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ export default function App() {
   const [monthly, setMonthly] = useState(500);
   // allow input to take the value of input and set to 0
   const [input, setInput] = useState();
+  const [expense, setExpense] = useState(" ");
 
-
-  const handleChange = ({target}) => {
+  const handleChange = ({ target }) => {
     const inputValue = target.value
     setInput(inputValue)
     console.log(inputValue)
@@ -22,39 +22,44 @@ export default function App() {
 
   const styles = {
     mainDiv: {
-      display: 'flex', 
-      justifyContent: 'space-evenly', 
+      display: 'flex',
+      justifyContent: 'space-evenly',
       width: "100vw"
     },
     form: {
-      display: "flex", 
+      display: "flex",
       flexDirection: "column"
     }
   }
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div style={styles.mainDiv}>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={styles.mainDiv}>
+          <div>
             <div>
-              <div>
-                <h1>Monthly Budget</h1>
-                <h2>{monthly}</h2>
-              </div>
-              <div>
-                <form style={styles.form} onChange={handleChange} onSubmit={returnBudget}>
-                  <input id="amount" placeholder='Amount'></input>
-                  <input placeholder="Expense"></input>
-                  <button type="submit"  >Add Expense</button>
-                </form>
-              </div>
+              <h1>Monthly Budget</h1>
+              <h2>{monthly}</h2>
             </div>
             <div>
-              <h1>Amount Saved</h1>
-              <h3>{input}</h3>
+              <form style={styles.form} onSubmit={returnBudget}>
+                <input id="amount" placeholder='Amount' onChange={handleChange}></input>
+                <input placeholder="Expense" onChange={handleChange}></input>
+                <input
+                  onChange={handleChange}
+                  type="date"
+                  name="bday"
+                />
+                <button type="submit"  >Add Expense</button>
+              </form>
             </div>
           </div>
-        </header>
-      </div>
-    );
-  }
+          <div>
+            <h1>Amount Saved</h1>
+            <h3>{input}</h3>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
